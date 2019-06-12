382c69b6ec7513c60ebf716aa4cf784428f73ad6
solareenlo <solareenlo@solareenlo.com>
2019-06-12 20:07:18 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+cp dist/deploy/* dist/iotajapan/
+cp dist/deploy/.* dist/iotajapan/
 
 # Build the project.
-rm -fr dist/iotajapan/*
-npm run build
+# rm -fr dist/iotajapan/*
+# npm run build
 
 # Go To Public folder
-cd dist/iotajapan
+# cd dist/iotajapan
 # Add changes to git.
-git add .
+# git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+# msg="rebuilding site `date`"
+# if [ $# -eq 1 ]
+#   then msg="$1"
+# fi
+# git commit -m "$msg"
 
 # Push source and build repos.
-git push origin HEAD:master
+# git push origin HEAD:master
 
 # Come Back up to the Project Root
-cd ../..
+# cd ../..
 
-git submodule update
-git add .
-git commit -m 'update'
-git push origin master
+# git submodule update
+# git add .
+# git commit -m 'update'
+# git push origin master
